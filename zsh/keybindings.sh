8afe3d96fc4bd80cb9284584b18383059b0907fc
bu6hunt3r <Felix1Koch@gmail.com>
2019-03-07 17:08:09 +0100

    Changed keybindings

--- zsh/keybindings.sh
+++ zsh/keybindings.sh
@@ -54,3 +54,18 @@
 	}
 	zle -N add_sudo
 	bindkey "^s" add_sudo
+
+# See https://github.com/51x/PTZ
+bindkey "^K"      kill-whole-line
+
+bindkey "${terminfo[khome]}" beginning-of-line
+bindkey "${terminfo[kend]}" end-of-line
+bindkey "\e[3~" delete-char
+
+bindkey -v
+bindkey '^R' history-incremental-search-backward
+bindkey '^T' history-incremental-search-backward
+
+# Jumping with ctrl+arrows
+bindkey "^[[1;5C" forward-word
+bindkey "^[[1;5D" backward-word
