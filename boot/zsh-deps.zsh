8d54234e99858e15c5aaebeb279d8ea005565603
bu6hunt3r <Felix1Koch@mail.com>
2019-06-05 16:54:25 +0200

    Removed dependencies within zsh_deps.zsh

--- boot/zsh-deps.zsh
+++ boot/zsh-deps.zsh
@@ -4,11 +4,11 @@ mkdir -p $zsh_deps && cd $zsh_deps
 
 # functionality 
 dotgrab ninrod/zsh-bd
-dotgrab ninrod/k
+# dotgrab ninrod/k
 
 # completion
-dotgrab ninrod/docker-zsh-completion
-dotgrab ninrod/docker-compose-zsh-completion
-dotgrab zsh-users/zsh-completions
-dotgrab ninrod/pass-zsh-completion
-dotgrab ninrod/gradle-zsh-completion
+#dotgrab ninrod/docker-zsh-completion
+#dotgrab ninrod/docker-compose-zsh-completion
+#dotgrab zsh-users/zsh-completions
+#dotgrab ninrod/pass-zsh-completion
+#dotgrab ninrod/gradle-zsh-completion
