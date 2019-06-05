ff232049515f3a0d5afb195c35cb58674ade141a
bu6hunt3r <Felix1Koch@mail.com>
2019-06-05 16:36:57 +0200

    minor

--- boot/functions.zsh
+++ boot/functions.zsh
@@ -1,7 +1,7 @@
 verifylink() {
     local symlink=${1:a}
     if [[ -e $symlink ]] && [[ ! -h $symlink ]]; then
-        echo -e "${Yellow}$symlink${Rst} is a ${Red}regular${Rst} file on your system."
+        echo -e "\t${Yellow}$symlink${Rst} is a ${Red}regular${Rst} file on your system."
         echo -e "Rename/backup the file ${Yellow}$symlink${Rst}, and run the script again."
         exit 1
     fi
@@ -12,12 +12,12 @@ updatelinks() {
     local symlink_old_target=${1:A}
     local symlink_new_target=${2:A}
     if [[ ! -h $symlink ]]; then
-        echo -e "$symlink ${Green}->${Rst} $symlink_new_target"
+        echo -e "\t$symlink ${Green}->${Rst} $symlink_new_target"
     elif [[ $symlink_old_target != $symlink_new_target ]]; then
         rm $symlink 
-        echo -e "$1 ${Red}-${Black}X${Rst}->${Rst} $symlink ${Green}->${Rst} $symlink_new_target"
+        echo -e "\t$1 ${Red}-${Black}X${Rst}->${Rst} $symlink ${Green}->${Rst} $symlink_new_target"
     else
-        echo -e "$1 ${Green}ok.${Rst}"
+        echo -e "\t$1 ${Green}ok.${Rst}"
         return 0
     fi
 
