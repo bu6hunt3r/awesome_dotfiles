a790231bea57df08fd1e885507915a3d339b3d91
bu6hunt3r <Felix1Koch@mail.com>
2019-06-06 13:15:11 +0200

    minor

--- zsh/prompt.zsh
+++ zsh/prompt.zsh
@@ -6,3 +6,19 @@ build_prompt_char() {
     local prompt_char="%(?.%F{magenta}.%F{red})${pc:-$}%f"
     echo -n "$prompt_char"
 }
+# default: showing whoami@hostname without truncation
+PROMPT="%F{blue}%1~%f %F{cyan}%n%f%F{red}@%f%F{yellow}%m%f $(build_prompt_char) "
+
+PROMPT_INFO_LEVEL=$SHELL_PROMPT_INFO_LEVEL
+
+# 0: showing only PWD with trunction
+# 1: showing @hostname and with truncation
+# 2: showing whaami@hostname and with truncation
+if [[ "${PROMPT_INFO_LEVEL}" == "0"  ]]; then
+  PROMPT="%F{blue}%15<...<%1~%<<%f $(build_prompt_char) "
+  # PROMPT="$(build_prompt_char) "
+elif [[ "${PROMPT_INFO_LEVEL}" == "1"  ]]; then
+  PROMPT="%F{blue}%13<...<%1~%<<%f %F{red}@%f%F{yellow}%m%f $(build_prompt_char) "
+elif [[ "${PROMPT_INFO_LEVEL}" == "2"  ]]; then
+  PROMPT="%F{blue}%13<...<%1~%<<%f %F{cyan}%n%f%F{red}@%f%F{yellow}%m%f $(build_prompt_char) "
+fi
