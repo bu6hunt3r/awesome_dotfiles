2e4fff4447c2882d68bb3109239387abd17990cf
bu6hunt3r <Felix1Koch@mail.com>
2019-06-06 13:15:01 +0200

    Loading ~/.options/shell.options.zsh in boot.zsh

--- boot/boot.zsh
+++ boot/boot.zsh
@@ -17,6 +17,9 @@ source $GIT_ROOT/boot/guard.zsh
 # defining functions verifylink build_git_url git_clone_error_msg dotgrab
 source $GIT_ROOT/boot/functions.zsh $1
 
+# Load custom shell options like prompt level, etc...
+source ~/.options/shell-options.zsh
+
 # Checking symlinks for repos within ~/.dotfiles
 source $GIT_ROOT/boot/symlinks.zsh
 
