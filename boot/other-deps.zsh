227f7cb7c1f2c31c23440f083ce017cc2efd66bf
bu6hunt3r <Felix1Koch@mail.com>
2019-06-12 16:52:22 +0200

    Added tmux-fingers dependency in other-deps

--- boot/other-deps.zsh
+++ boot/other-deps.zsh
@@ -4,3 +4,6 @@ mkdir -p $other_deps && cd $other_deps
 dotgrab seebi/dircolors-solarized
 verifylink ~/.lscolors
 updatelinks ~/.lscolors $other_deps/seebi/dircolors-solarized/dircolors.256dark
+
+# grab tmux fingers
+dotgrab Morantron/tmux-fingers
