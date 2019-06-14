66c7eaef3e2c7f0e5bf7e7c82a94b3713c322d67
bu6hunt3r <Felix1Koch@mail.com>
2019-06-14 10:50:13 +0200

    Changed init-el

--- emacs/init.el
+++ emacs/init.el
@@ -1,24 +1,3 @@
-(let ((gc-cons-threshold (* 25 1024 1024)))
-    (require 'package)
-    (setq package-enable-at-startup nil)
-    (defun bu6hunt3r--rsyncfy (repo) (concat "~/.dotfiles/deps/emacs/ninrod/rsynced-melpa/" repo))
-    (setq package-archives `(("melpa"   . ,(bu6hunt3r--rsyncfy "melpa/"))
-                             ("org"     . ,(bu6hunt3r--rsyncfy "org/"))
-                             ("gnu"     . ,(bu6hunt3r--rsyncfy "gnu/"))))
-    (package-initialize)
-    (unless (package-installed-p 'use-package))
-        (package-refresh-contents)
-        (package-install 'use-package)
-    
-    (use-package diminish :ensure t)
-
-    ;;(use-package org
-    ;;    :pin org
-    ;;    :ensure org-plus-contrib
-    ;;    :defer 7)  ;; fetch latest version of `org-mode'
-    
-    (setq vc-follow-symlinks "t")
-
 (setq org-capture-templates
       '(("t" "Todo" entry (file+headline "~/org/gtd.org" "Tasks")
          "* TODO %?\n  %i\n  %a")
