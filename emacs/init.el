f382d1fdf69d66ecb0a5c75222b97d266f662b0c
bu6hunt3r <bu6hunt3r@web.de>
2019-10-01 13:05:59 +0200

    Cleaned init.el

--- emacs/init.el
+++ emacs/init.el
@@ -1,17 +1,15 @@
-(when (not package-archive-contents)
-    (package-refresh-contents))
-
-(require 'package)
+(package-initialize)
 (add-to-list 'package-archives
              '("melpa" . "http://melpa.org/packages/"))
-(when (< emacs-major-version 24)
-  ;; For important compatibility libraries like cl-lib
-  (add-to-list 'package-archives '("gnu" . "http://elpa.gnu.org/packages/")))
-(package-initialize) 
+(add-to-list 'package-archives
+             '("org" . "http://orgmode.org/elpa/"))
 
-(unless (package-installed-p 'use-package)
+;; install any packages in jpk-packages, if they are not installed already
+(let ((refreshed nil))
+  (when (not package-archive-contents)
     (package-refresh-contents)
-      (package-install 'use-package))
+	(package-install 'use-package)
+    (setq refreshed t)))
 
 (require 'use-package)
 (setq use-package-always-ensure t)
@@ -21,6 +19,31 @@
 (use-package neotree)
 (use-package org-brain)
 (use-package monokai-theme)
+;; (package-refresh-contents)
+;; 
+;; (require 'package)
+;; (add-to-list 'package-archives
+;;              '("melpa" . "http://melpa.org/packages/"))
+;; (when (< emacs-major-version 24)
+;; ;; For important compatibility libraries like cl-lib
+;; (add-to-list 'package-archives '("gnu" . "http://elpa.gnu.org/packages/")))
+;; (package-initialize) 
+;; ;; load font settings functionality if in GUI mode
+;; (add-to-list 'load-path "~/.emacs.d")
+
+;; theme settings
+(add-to-list 'custom-theme-load-path "~/.emacs.d/themes/")
+(load-theme `tron-legacy t)
+
+;; font settings
+(set-default-font "IBM 3270 Medium")
+(load-file "~/.emacs.d/font-size.el")
+(require 'font-size)
+(font-size-init 14)
+(define-key global-map (kbd "M-+") 'font-size-increase)
+(define-key global-map (kbd "M--") 'font-size-decrease)
+(define-key global-map (kbd "M-0") 'font-size-default)
+
 
 (setq org-capture-templates
       '(("t" "Todo" entry (file+headline "~/org/gtd.org" "Tasks")
@@ -31,10 +54,10 @@
 ;; set neotree key binding
 (require 'neotree)
 (global-set-key [f12] 'neotree-toggle)
-
-;; set gruvbox theme
-;; (load-theme 'gruvbox t)
-
+;; 
+;; ;; set gruvbox theme
+;; ;; (load-theme 'gruvbox t)
+;; 
 (custom-set-variables
  ;; custom-set-variables was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
@@ -43,43 +66,45 @@
  '(custom-safe-themes
    (quote
     ("9d36e7cbea9ab075fa1920275cbde349f5b80c9b901500d296856142b32c7516" "1436d643b98844555d56c59c74004eb158dc85fc55d2e7205f8d9b8c860e177f" default)))
- '(inhibit-startup-screen t)
- '(package-selected-packages
-   (quote
-    (rainbow-delimiters slime geiser ac-etags ox-wk ## company-ghc ghc company-jedi haskell-mode auctex-latexmk ein latex-math-preview monokai-theme ascii-art-to-unicode helm gruvbox-theme org-brain org htmlize neotree))))
+'(font-size--current-size size)
+'(font-size--default-size default-size)
+'(inhibit-startup-screen t)
+  '(package-selected-packages
+    (quote
+     (doom-themes use-package rainbow-delimiters slime geiser ac-etags ox-wk ## company-ghc ghc company-jedi haskell-mode auctex-latexmk ein latex-math-preview monokai-theme ascii-art-to-unicode helm gruvbox-theme org-brain org htmlize neotree))))
 (custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- )
-
+;; custom-set-faces was added by Custom.
+;; If you edit it by hand, you could mess it up, so be careful.
+;; Your init file should contain only one such instance.
+;; If there is more than one, they won't work right.
+)
+;; 
 (setq org-support-shift-select 'always)
-
-;; Customize doom-theme
-(require 'doom-themes)
-
-;; Global settings (defaults)
-(setq doom-themes-enable-bold t    ; if nil, bold is universally disabled
-            doom-themes-enable-italic t) ; if nil, italics is universally disabled
-
-;; Load the theme (doom-one, doom-molokai, etc); keep in mind that each theme
-;; may have their own settings.
-;;(load-theme 'doom-one t)
-
-;; Load monokai theme
-;;(load-theme 'monokai t)
-
-;; Enable flashing mode-line on errors
-(doom-themes-visual-bell-config)
-
-;; Enable custom neotree theme (all-the-icons must be installed!)
-;;(doom-themes-neotree-config)
-;; or for treemacs users
-(doom-themes-treemacs-config)
-
-;; Corrects (and improves) org-mode's native fontification.
-(doom-themes-org-config)
+;; 
+;; ;; Customize doom-theme
+;; (require 'doom-themes)
+;; 
+;; ;; Global settings (defaults)
+;; (setq doom-themes-enable-bold t    ; if nil, bold is universally disabled
+;;             doom-themes-enable-italic t) ; if nil, italics is universally disabled
+;; 
+;; ;; Load the theme (doom-one, doom-molokai, etc); keep in mind that each theme
+;; ;; may have their own settings.
+;; ;;(load-theme 'doom-one t)
+;; 
+;; ;; Load monokai theme
+;; ;;(load-theme 'monokai t)
+;; 
+;; ;; Enable flashing mode-line on errors
+;; (doom-themes-visual-bell-config)
+;; 
+;; ;; Enable custom neotree theme (all-the-icons must be installed!)
+;; ;;(doom-themes-neotree-config)
+;; ;; or for treemacs users
+;; (doom-themes-treemacs-config)
+;; 
+;; ;; Corrects (and improves) org-mode's native fontification.
+;; (doom-themes-org-config)
 
 ;; ascii-art-to-unicode settings
 (defface aa2u-face '((t . nil))
@@ -94,28 +119,28 @@
     (ignore-errors (aa2u (point-min) (point-max)))))
 (with-eval-after-load 'org-brain
   (add-hook 'org-brain-after-visualize-hook #'aa2u-org-brain-buffer))
-
-
-(if nil 
-  (display-graphic-p)
-    (set-background-color "272728282222"))
-;; set transparency
-(set-frame-parameter (selected-frame) 'alpha '(85 85))
-(add-to-list 'default-frame-alist '(alpha 85 85))
-
+;; 
+;; 
+;; ;;(if nil 
+;; ;;  (display-graphic-p)
+;; ;;    (set-background-color "272728282222"))
+;; ;; set transparency
+;; ;;(set-frame-parameter (selected-frame) 'alpha '(85 85))
+;; ;;(add-to-list 'default-frame-alist '(alpha 85 85))
+;; 
 (menu-bar-mode -1)
-
+;; Language hooks
+(add-hook 'after-init-hook 'global-company-mode)
 ;; init ghc autocmplete
 (require 'package)
-(add-to-list 'package-archives 
-         '("melpa" . "http://melpa-stable.milkbox.net/packages/"))
-(package-initialize)
+;; (add-to-list 'package-archives 
+;;          '("melpa" . "http://melpa-stable.milkbox.net/packages/"))
+;; (package-initialize)
 (autoload 'ghc-init "ghc" nil t)
 (autoload 'ghc-debug "ghc" nil t)
 (add-hook 'haskell-mode-hook (lambda () (ghc-init)))
-
-(add-hook 'after-init-hook 'global-company-mode)
-; No delay in showing suggestions.
+;; 
+;; No delay in showing suggestions.
 (setq company-idle-delay 0)
 ; Show suggestions after entering one character.
 (setq company-minimum-prefix-length 1)
@@ -130,9 +155,10 @@
 
 (add-to-list 'company-backends 'company-ghc)
 
-;;(load-file "~/.emacs.d/elpa/cheatsheet.el")
-;;(load-file "~/.emacs.d/cheats.el")
+;; loading cheatsheets
+(load-file "~/.emacs.d/cheats.el")
 
+;; Other window backwards
 (defun other-window-backward ()
   "Select the previous window."
   (interactive)
@@ -142,41 +168,48 @@
 ;; ansi color codes
 (add-hook 'shell-mode-hook 'ansi-color-for-comint-mode-on)
 (add-to-list 'comint-output-filter-functions 'ansi-color-process-output)
-
 ;; gdb: Cycle backwards through input history, saving input.
 (eval-after-load "gud"
   '(progn 
      (define-key gud-mode-map (kbd "<up>") 'comint-previous-input)
      (define-key gud-mode-map (kbd "<down>") 'comint-next-input)))
 
+;; Kill lines backward
 (defun backward-kill-line (arg)
   "Kill ARG lines backward."
   (interactive "p")
   (kill-line (- 1 arg)))
 
 (global-set-key "\C-c u" 'backward-kill-line)
-
-;; loading transpose-lines feature (bound to "C-M-up/down")
-;;(load-file "~/.emacs.d/vendor/transpose-lines.el")
-
+;; 
+;; ;; loading transpose-lines feature (bound to "C-M-up/down")
+;; ;;(load-file "~/.emacs.d/vendor/transpose-lines.el")
+;; 
 ;; mu4e settings
 (add-to-list 'load-path "/usr/share/emacs/site-lisp/mu4e")
 (load-file "/home/cr0c0/.emacs.d/mu4e-settings.el")
 
 ;; emojify (useful for telegram)
 (add-hook 'after-init-hook #'global-emojify-mode)
-
-(autoload 'multi-term "multi-term" nil t)
-(autoload 'multi-term-next "multi-term" nil t)
-
-;; (setq multi-term-program "/bin/bash")   ;; use bash
-(setq multi-term-program "/bin/zsh") ;; or use zsh...
+;; 
+;; (autoload 'multi-term "multi-term" nil t)
+;; (autoload 'multi-term-next "multi-term" nil t)
+;; 
+;; ;; (setq multi-term-program "/bin/bash")   ;; use bash
+;; (setq multi-term-program "/bin/zsh") ;; or use zsh...
+;; 
 
 ;; only needed if you use autopair
 (add-hook 'term-mode-hook
   #'(lambda () (setq autopair-dont-activate t)))
+;; 
+;; 
+;; (global-set-key (kbd "C-c t") 'multi-term-next)
+;; (global-set-key (kbd "C-c T") 'multi-term) ;; create a new one
 
-
-(global-set-key (kbd "C-c t") 'multi-term-next)
-(global-set-key (kbd "C-c T") 'multi-term) ;; create a new one
-
+;; Cask settings
+(require 'cask "/home/cr0c0/cask/cask.el")
+;; (cask-initialize)
+;; 
+(require 'pallet)
+(pallet-mode t)
