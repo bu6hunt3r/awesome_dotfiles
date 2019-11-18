838f7fbc48c5d9e8a801f9cedf58de61095d85a0
Dave Eddy <dave@daveeddy.com>
2019-11-18 14:56:03 +0100

    Added tldr config / emacs features / zsh features

--- zsh/main.zsh
+++ zsh/main.zsh
@@ -13,7 +13,7 @@ source ~/.zsh.d/fixescapecodes.zsh
 # megabytes / kilobytes measurement & faster dir creation & bc utility
 source ~/.zsh.d/functions.zsh
 # load diverse aliases
-source ~/.zsh.d/aliases.zsh
+# source ~/.zsh.d/aliases.zsh
 # load git aliases
 source ~/.zsh.d/git-aliases.zsh
 # load docker aliases
@@ -41,5 +41,82 @@ source ~/.zsh.d/fzf.zsh
 
 source ~/.zsh.d/syntax-highlighting.zsh
 [ -f ~/.fzf.zsh ] && source ~/.fzf.zsh
-# ctf-tools: PATH setup
-export PATH=/home/cr0c0/dev/ctf-tools/bin:$PATH
+
+echo -e "\033[1;33m==== Loading aliases ====\033[0m"
+lsd_installed=$(which lsd &>/dev/null)
+if [[ -z $lsd_installed ]]; then
+		lsd_prompt="\033[1;32m- [√] lsd installed...\033[0m"
+		echo "alias ls='lsd'"  >> ~/.zsh.d/aliases.zsh
+		echo "alias l='ls -l'" >> ~/.zsh.d/aliases.zsh
+		echo "alias la='ls -a'"  >> ~/.zsh.d/aliases.zsh
+		echo "alias lla='ls -la'" >> ~/.zsh.d/aliases.zsh
+		echo "alias lt='ls --tree'" >> ~/.zsh.d/aliases.zsh
+else
+		lsd_prompt="\033[1;31m- [ ] lsd not installed...(also check for patched font -> https://github.com/ryanoasis/nerd-fonts/blob/master/readme.md)\033[0m"
+fi
+
+echo -e "$lsd_prompt"
+
+# load diverse aliases
+source ~/.zsh.d/aliases.zsh
+
+# load tmux aliases
+source ~/.config/zsh/.tmux_aliases
+if [[ $? -eq 0 ]]; then
+		echo -e "\033[1;32m- [√] tmux aliases loaded...\033[0m"
+else
+		echo -e "\033[1;31m- [ ] tmux aliases not loaded...\033[0m"
+fi
+
+# load virtualenv aliases
+source ~/.config/zsh/.python_functions
+if [[ $? -eq 0 ]]; then
+		echo -e "\033[1;32m- [√] virtualenv aliases loaded...\033[0m"
+else
+		echo -e "\033[1;31m- [ ] virtualenv aliases not loaded...\033[0m"
+fi
+
+
+case "$(uname -s)" in
+    Linux)
+    if [[ -f "$HOME/.local/bin/virtualenvwrapper.sh" ]]; then
+			virtualenvwrapper_prompt="\033[1;32m- [√] virtualenvwrapper installed...\033[0m"
+			source "$HOME/.local/bin/virtualenvwrapper.sh"
+		else
+      virtualenvwrapper_prompt="\033[1;31m- [ ] virtualenvwrapper not installed...\033[0m"
+		fi
+		;;
+esac
+
+echo -e "$virtualenvwrapper_prompt"
+
+echo ""
+echo "\033[1;33m==== Loading zsh's config files (stow actions) ====\033[0m"
+source "$HOME/.config/zsh/.zsh_path"
+if [ $? -eq 0 ]; then
+		echo -e "\033[1;32m- [√] loaded ~/.config/zsh/.zsh_path...(check within ~/.zshrc)\033[0m"
+else
+		echo -e "\033[1;31m- [ ] did not load ~/.config/zsh/.zsh_path...something went wrong...(check within ~/.zshrc)\033[0m"
+fi
+
+# General checks
+echo ""
+echo -e "\033[1;33m==== General checks ====\033[0m"
+brew_installed=$(which brew &>/dev/null)
+if [[ -z $brew_installed ]]; then
+		brew_prompt="\033[1;32m- [√] brew installed...\033[0m"
+else
+		brew_prompt="\033[1;31m- [ ] brew not installed...\033[0m"
+fi
+echo -e "$brew_prompt"
+
+tig_installed=$(which tig &>/dev/null)
+if [[ -z $tig_installed ]]; then
+		tig_prompt="\033[1;32m- [√] tig installed...\033[0m"
+else
+		tig_prompt="\033[1;31m- [ ] tig not installed...\033[0m"
+fi
+echo -e "$tig_prompt"
+
+
+export ZSH_LOADED=1
