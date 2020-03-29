78488d65d1e41839d52e99f187f0611f10fe5632
bu6hunt3r <bu6hunt3r@web.de>
2020-03-29 19:28:07 +0200

    expanded zsh/functions

--- common/zsh/functions.zsh
+++ common/zsh/functions.zsh
@@ -65,3 +65,11 @@ function fkill() {
 		echo $pid | xargs kill -${1:-9}
 	fi
 }
+
+function gdb-tmux() {
+    local id="$(tmux split-pane -hPF "#D" "tail -f /dev/null")"
+    tmux last-pane
+    local tty="$(tmux display-message -p -t "$id" '#{pane_tty}')"
+    gdb -ex "dashboard -output $tty" "$@"
+    tmux kill-pane -t "$id"
+}
